34f1d7e4ecb8183a3823e27e30f48dc930970d51
abhi9065 <110813322+abhi9065@users.noreply.github.com>
2022-10-05 16:06:15 +0530

    update

--- doms.js
+++ doms.js
@@ -1,5 +1,13 @@
-var element
+var element;
 
-document.querySelector(".list-non-bullet").innerHTML;
-element = document.querySelector()
-console.log(element);
+document.getElementById("abhi").addEventListener('click',abc)
+
+
+
+
+function abc(){
+//this.classList.toggle("xyz");
+
+var a = this.classList.item("1")
+   console.log(a)
+}
